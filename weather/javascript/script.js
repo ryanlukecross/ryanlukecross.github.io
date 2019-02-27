850d111d0a9d1bd73743f99a07f678e78912144b
ryanlukecross <ryanlukecross1@gmail.com>
2019-02-27 10:48:30 -0700

    point

---
+++ weather/javascript/script.js
@@ -0,0 +1,43 @@
+function windDial(direction){
+    const dial = document.getElementById("dial");
+
+    // Determine the dial class
+ switch (direction){
+    case "North":
+    case "N":
+     dial.setAttribute("class", "n"); //"n" is the CSS rule selector
+     break;
+    case "NE":
+    case "NNE":
+    case "ENE":
+     dial.setAttribute("class", "ne");
+     break;
+    case "NW":
+    case "NNW":
+    case "WNW":
+     dial.setAttribute("class", "nw");
+     break;
+    case "South":
+    case "S":
+     dial.setAttribute("class", "s");
+     break;
+    case "SE":
+    case "SSE":
+    case "ESE":
+     dial.setAttribute("class", "se");
+     break;
+    case "SW":
+    case "SSW":
+    case "WSW":
+     dial.setAttribute("class", "sw");
+     break;
+    case "East":
+    case "E":
+     dial.setAttribute("class", "e");
+     break;
+    case "West":
+    case "W":
+     dial.setAttribute("class", "w");
+     break;
+   }
+}
