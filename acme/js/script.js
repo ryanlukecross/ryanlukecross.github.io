acd6ee198f074c0e0123265d49f13779fad7fe4e
ryanlukecross <ryanlukecross1@gmail.com>
2019-04-01 12:22:03 -0600

    Different divs for each link in the nav bar (acme)

--- acme/js/script.js
+++ acme/js/script.js
@@ -15,7 +15,7 @@ function BuildNav(URL) {
          throw new ERROR('Network response was not OK.');
       }).then(function (data) {
          let tom = data.Titles;
-         content += "<li><a href='#'>Home</a></li><li><a href='#'>" + tom.first + "</a></li><li><a href='#'>" + tom.second + "</a></li><li><a href='#'>" + tom.third + "</a></li><li><a href='#'>" + tom.fourth + "</a></li>";
+         content += "<li><a href='#'>Home</a></li><li><a href='#'>" + tom.first + "</a></li><li><button type='button' id='anvil-button' onclick='switchAnvils()'>" + tom.second + "</button></li><li><a href='#'>" + tom.third + "</a></li><li><a href='#'>" + tom.fourth + "</a></li>";
          document.getElementById('nav-ul').innerHTML = content;
          console.log("The navigation bar was created by Javascript here AS: " + content);
       })
@@ -24,4 +24,49 @@ function BuildNav(URL) {
          alert('Sorry, the data could not be processed.');
       })
    document.getElementById("nav-ul").innerHTML = content;
+}
+
+function switchHome()
+{
+   document.getElementById('anvils-content').setAttribute('class', 'hide');
+   document.getElementById('explosives-content').setAttribute('class', 'hide');
+   document.getElementById('decoys-content').setAttribute('class', 'hide');
+   document.getElementById('traps-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', '');
+}
+
+function switchAnvils()
+{
+   document.getElementById('anvils-content').setAttribute('class', '');
+   document.getElementById('explosives-content').setAttribute('class', 'hide');
+   document.getElementById('decoys-content').setAttribute('class', 'hide');
+   document.getElementById('traps-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', 'hide');
+}
+
+function switchExplosives()
+{
+   document.getElementById('anvils-content').setAttribute('class', 'hide');
+   document.getElementById('explosives-content').setAttribute('class', '');
+   document.getElementById('decoys-content').setAttribute('class', 'hide');
+   document.getElementById('traps-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', 'hide');
+}
+
+function switchDecoys()
+{
+   document.getElementById('anvils-content').setAttribute('class', 'hide');
+   document.getElementById('explosives-content').setAttribute('class', 'hide');
+   document.getElementById('decoys-content').setAttribute('class', '');
+   document.getElementById('traps-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', 'hide');
+}
+
+function switchTraps()
+{
+   document.getElementById('anvils-content').setAttribute('class', 'hide');
+   document.getElementById('explosives-content').setAttribute('class', 'hide');
+   document.getElementById('decoys-content').setAttribute('class', 'hide');
+   document.getElementById('traps-content').setAttribute('class', '');
+   document.getElementById('home-content').setAttribute('class', 'hide');
 }
