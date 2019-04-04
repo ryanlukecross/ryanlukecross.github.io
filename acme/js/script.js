5518e178211f9bcece256b2831d516aa79405437
ryanlukecross <ryanlukecross1@gmail.com>
2019-04-04 16:51:53 -0600

    Pushing

--- acme/js/script.js
+++ acme/js/script.js
@@ -15,11 +15,12 @@ function buildNav(URL) {
          throw new ERROR('Network response was not OK.');
       }).then(function (data) {
          let tom = data.Titles;
-         content += "<li><button type='button' id='home-button' onclick='switch"        + "Home"     + "()'>" + "Home"     + "</button></li> " +
-                    "<li><button type='button' id='anvil-button' onclick='switch"       + tom.first  + "()'>" + tom.first  + "</button></li>" + 
-                    "<li><button type='button' id='explosives-button' onclick='switch"  + tom.first  + "()'>" + tom.second + "</button></li>" + 
-                    "<li><button type='button' id='decoys-button' onclick='switch"      + tom.first  + "()'>" + tom.third  + "</button></li>" + 
-                    "<li><button type='button' id='traps-button' onclick='switch"       + tom.first  + "()'>" + tom.fourth + "</button></li>";
+         console.log("This is tom: " + data);
+         content += "<li><button type='button' id='home-button' onclick='switchTo('" + "Home" + "')'>" + "Home" + "</button></li> " +
+            "<li><button type='button' id='anvil-button' onclick='switchTo('" + tom.first + "')'>" + tom.first + "</button></li>" +
+            "<li><button type='button' id='explosives-button' onclick='switchTo('" + tom.second + "')'>" + tom.second + "</button></li>" +
+            "<li><button type='button' id='decoys-button' onclick='switchTo('" + tom.third + "')'>" + tom.third + "</button></li>" +
+            "<li><button type='button' id='traps-button' onclick='switchTo('" + tom.fourth + "')'>" + tom.fourth + "</button></li>";
          document.getElementById('nav-ul').innerHTML = content;
          console.log("The navigation bar was created by Javascript here AS: " + content);
       })
@@ -30,52 +31,76 @@ function buildNav(URL) {
    document.getElementById("nav-ul").innerHTML = content;
 }
 
-function switchHome()
-{
-   document.getElementById('anvils-content').setAttribute('class', 'hide');
-   document.getElementById('explosives-content').setAttribute('class', 'hide');
-   document.getElementById('decoys-content').setAttribute('class', 'hide');
-   document.getElementById('traps-content').setAttribute('class', 'hide');
-   document.getElementById('home-content').setAttribute('class', '');
+
+function switchTo(type) {
+   alert(type);
+   if (type == 'home') {
+      document.getElementById('home-content').setAttribute('class', '');
+      document.getElementById('more-basic-than-basic').setAttribute('class', 'hide');
+   } else {
+      buildPage(type);
+      document.getElementById('home-content').setAttribute('class', 'hide');
+      document.getElementById('more-basic-than-basic').setAttribute('class', '');
+   }
    buildNav(jsonURL);
 }
 
-function switchAnvils()
+function buildPage(type) {
+   let content = "";
+   fetch(URL)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         }
+         throw new ERROR('Network response was not OK.');
+      }).then(function (data) {
+         let tom = data.Titles;
+         let Home = "home";
+         let Anvils = "anvils";
+         let Explosives = "explosives";
+         let Decoys = "decoys";
+         let Traps = "traps";
+         content += "<li><button type='button' id='home-button' onclick='switchTo(" + "Home" + ")'>" + "Home" + "</button></li> " +
+            "<li><button type='button' id='anvil-button' onclick='switchTo(" + tom.first + ")'>" + tom.first + "</button></li>" +
+            "<li><button type='button' id='explosives-button' onclick='switchTo(" + tom.second + ")'>" + tom.second + "</button></li>" +
+            "<li><button type='button' id='decoys-button' onclick='switchTo(" + tom.third + ")'>" + tom.third + "</button></li>" +
+            "<li><button type='button' id='traps-button' onclick='switchTo(" + tom.fourth + ")'>" + tom.fourth + "</button></li>";
+         document.getElementById('nav-ul').innerHTML = content;
+         console.log("The navigation bar was created by Javascript here AS: " + content);
+      })
+      .catch(function (error) {
+         console.log('There was a fetch problem: ', error.message);
+         alert('Sorry, the data could not be processed.');
+      })
+   document.getElementById("nav-ul").innerHTML = content;
+}
+/*
+function switchHome()
 {
-   document.getElementById('anvils-content').setAttribute('class', '');
-   document.getElementById('explosives-content').setAttribute('class', 'hide');
-   document.getElementById('decoys-content').setAttribute('class', 'hide');
-   document.getElementById('traps-content').setAttribute('class', 'hide');
-   document.getElementById('home-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', '');
+   document.getElementById('more-basic-than-basic').setAttribute('class', );
    buildNav(jsonURL);
 }
+
 
 function switchExplosives()
 {
-   document.getElementById('anvils-content').setAttribute('class', 'hide');
-   document.getElementById('explosives-content').setAttribute('class', '');
-   document.getElementById('decoys-content').setAttribute('class', 'hide');
-   document.getElementById('traps-content').setAttribute('class', 'hide');
-   document.getElementById('home-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', '');
+   document.getElementById('more-basic-than-basic').setAttribute('class', );
    buildNav(jsonURL);
 }
 
 function switchDecoys()
 {
-   document.getElementById('anvils-content').setAttribute('class', 'hide');
-   document.getElementById('explosives-content').setAttribute('class', 'hide');
-   document.getElementById('decoys-content').setAttribute('class', '');
-   document.getElementById('traps-content').setAttribute('class', 'hide');
-   document.getElementById('home-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', '');
+   document.getElementById('more-basic-than-basic').setAttribute('class', );
    buildNav(jsonURL);
 }
 
 function switchTraps()
 {
-   document.getElementById('anvils-content').setAttribute('class', 'hide');
-   document.getElementById('explosives-content').setAttribute('class', 'hide');
-   document.getElementById('decoys-content').setAttribute('class', 'hide');
-   document.getElementById('traps-content').setAttribute('class', '');
-   document.getElementById('home-content').setAttribute('class', 'hide');
+   document.getElementById('home-content').setAttribute('class', '');
+   document.getElementById('more-basic-than-basic').setAttribute('class', );
    buildNav(jsonURL);
 }
+*/
