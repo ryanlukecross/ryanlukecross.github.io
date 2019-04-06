88de73995ec9015cdd8270a9316b7a6ce3ec01c9
ryanlukecross <ryanlukecross1@gmail.com>
2019-04-05 21:30:32 -0600

    Acme

--- acme/js/script.js
+++ acme/js/script.js
@@ -1,12 +1,8 @@
 /* Javascript file for the ACME Final Project CIT230 */
-
 console.log("script.js is being read");
 // Set the path to the JSON file and call the build nav file
 const jsonURL = '/acme/js/acme.json';
 buildNav(jsonURL);
-
-document.getElementById('more-basic-than-basic').setAttribute('class', 'hide');
-
 function buildNav(URL) {
    let content = "";
    fetch(URL)
@@ -17,11 +13,11 @@ function buildNav(URL) {
          throw new ERROR('Network response was not OK.');
       }).then(function (data) {
          let tom = data.Titles;
-         content += "<li><button type='button' id='home-button' onclick='switch"        + "Home"     + "()'>" + "Home"     + "</button></li> " +
-                    "<li><button type='button' id='anvil-button' onclick='switch"       + tom.first  + "()'>" + tom.first  + "</button></li>" + 
-                    "<li><button type='button' id='explosives-button' onclick='switch"  + tom.second  + "()'>" + tom.second + "</button></li>" + 
-                    "<li><button type='button' id='decoys-button' onclick='switch"      + tom.third  + "()'>" + tom.third  + "</button></li>" + 
-                    "<li><button type='button' id='traps-button' onclick='switch"       + tom.fourth  + "()'>" + tom.fourth + "</button></li>";
+         content += "<li><button type='button' id='home-button' onclick='switch" + "Home" + "()'>" + "Home" + "</button></li> " +
+            "<li><button type='button' id='anvil-button' onclick='switch" + tom.first + "()'>" + tom.first + "</button></li>" +
+            "<li><button type='button' id='explosives-button' onclick='switch" + tom.second + "()'>" + tom.second + "</button></li>" +
+            "<li><button type='button' id='decoys-button' onclick='switch" + tom.third + "()'>" + tom.third + "</button></li>" +
+            "<li><button type='button' id='traps-button' onclick='switch" + tom.fourth + "()'>" + tom.fourth + "</button></li>";
          document.getElementById('nav-ul').innerHTML = content;
          console.log("The navigation bar was created by Javascript here AS: " + content);
       })
@@ -31,43 +27,31 @@ function buildNav(URL) {
       })
    document.getElementById("nav-ul").innerHTML = content;
 }
-
-function switchHome()
-{
+function switchHome() {
    document.getElementById('home-content').setAttribute('class', '');
    document.getElementById('more-basic-than-basic').setAttribute('class', 'hide');
 }
-
-function switchAnvils()
-{
+function switchAnvils() {
    buildPage(jsonURL, "Anvils");
    document.getElementById('home-content').setAttribute('class', 'hide');
    document.getElementById('more-basic-than-basic').setAttribute('class', '');
 }
-
-function switchExplosives()
-{
+function switchExplosives() {
    buildPage(jsonURL, "Explosives");
    document.getElementById('home-content').setAttribute('class', 'hide');
    document.getElementById('more-basic-than-basic').setAttribute('class', '');
 }
-
-function switchDecoys()
-{
+function switchDecoys() {
    buildPage(jsonURL, 'Decoys');
    document.getElementById('home-content').setAttribute('class', 'hide');
    document.getElementById('more-basic-than-basic').setAttribute('class', '');
 }
-
-function switchTraps()
-{
+function switchTraps() {
    buildPage(jsonURL, "Traps");
    document.getElementById('home-content').setAttribute('class', 'hide');
    document.getElementById('more-basic-than-basic').setAttribute('class', '');
 }
-
 function buildPage(URL, type) {
-   let content = "";
    fetch(URL)
       .then(function (response) {
          if (response.ok) {
@@ -84,7 +68,6 @@ function buildPage(URL, type) {
          document.getElementById("basic-info-1").innerHTML = "<strong>Made by: </strong>" + d['manufacturer'];
          document.getElementById("basic-info-2").innerHTML = "<strong>Reviews: </strong>" + d['reviews'] + " stars";
          document.getElementById("basic-price").innerHTML = "Price: $" + d['price'];
-
       })
       .catch(function (error) {
          console.log('There was a fetch problem: ', error.message);
